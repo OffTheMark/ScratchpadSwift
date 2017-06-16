804fcd75ddc2d47d051557a90338c96dc031075e
Marc-Antoine Malepart <mamalepart@solutionsmedias360.com>
2017-06-16 16:05:55 -0400

    Setup Firebase

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Scratchpad' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Scratchpad
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+
+end
---
+++ Scratchpad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scratchpad
+//
+//  Created by Marc-Antoine Malepart on 2017-06-16.
+//  Copyright © 2017 Marc-Antoine Malépart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
