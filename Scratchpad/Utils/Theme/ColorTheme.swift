4543a851655e9a89ed01f2ab6e1fc1e0faeac77d
Marc-Antoine Malepart <mamalepart@solutionsmedias360.com>
2017-07-24 13:42:09 -0400

    Format code

--- Scratchpad/Utils/Theme/ColorTheme.swift
+++ Scratchpad/Utils/Theme/ColorTheme.swift
@@ -3,15 +3,15 @@ import UIKit
 
 struct ColorTheme {
 	// MARK:- Background
-	
-	static let primaryBackground = UIColor(red: 0.0/255.0, green: 95.0/255.0, blue: 154.0/255.0, alpha: 1)
-	static let accentBackground = UIColor.red
-	static let lightBackground = UIColor(red: 0.94, green: 0.94, blue: 0.94, alpha: 1)
-	static let whiteBackground = UIColor.white
-	
+
+	static let primaryBackground = UIColor(red: 0.0 / 255.0, green: 95.0 / 255.0, blue: 154.0 / 255.0, alpha: 1)
+	static let accentBackground  = UIColor.red
+	static let lightBackground   = UIColor(red: 0.94, green: 0.94, blue: 0.94, alpha: 1)
+	static let whiteBackground   = UIColor.white
+
 	// MARK:- Text
-	
+
 	static let textOnPrimary = UIColor.white
-	static let darkText = UIColor.darkGray
-	static let errorText = UIColor.red
+	static let darkText      = UIColor.darkGray
+	static let errorText     = UIColor.red
 }
